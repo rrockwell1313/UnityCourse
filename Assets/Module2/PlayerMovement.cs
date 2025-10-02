f75f5e0b4b846e1fdd8ad3c5e9969fd2f7fa34ae
rrockwell1313 <122168611+rrockwell1313@users.noreply.github.com>
2025-10-01 20:19:27 -0500

    Module2 changes
    NNNNEW MIKE

--- Assets/Module2/PlayerMovement.cs
+++ Assets/Module2/PlayerMovement.cs
@@ -1,46 +1,74 @@
-using System;
 using UnityEngine;
 
-// Simple demo script for Module2 showing variables, lifecycle methods, and basic movement
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    //primitive variables.
+    //float, int, double, char, bool, string, --
+   //camelCasing, WhatEverCasing, _localCasing, -LocalCasing
+  public float moveSpeed = 10f;
 
-    void Start()
+  void Start()
     {
-
+      
     }
 
     void Update()
     {
-        HandleRawMovement();
+
+      HandleRawMovement();
     }
 
-    private void HandleRawMovement()
+  private void HandleRawMovement()
+  {
+    if (Input.GetKey(KeyCode.W))
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = transform.position + Vector3.forward * (moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = transform.position + Vector3.back * (moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position + Vector3.left * (moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = transform.position + Vector3.right * (moveSpeed * Time.deltaTime);
-        }
+      transform.position = transform.position + Vector3.forward * (moveSpeed * Time.deltaTime);
     }
-    
-    private void HandleMovement()
+    if (Input.GetKey(KeyCode.S))
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 moveInput = new(horizontal, 0f, vertical);
-        transform.position = transform.position + moveInput * (moveSpeed * Time.deltaTime);
+      transform.position = transform.position + Vector3.back * (moveSpeed * Time.deltaTime);
     }
+    if (Input.GetKey(KeyCode.A))
+    {
+      transform.position = transform.position + Vector3.left * (moveSpeed * Time.deltaTime);
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      transform.position = transform.position + Vector3.right * (moveSpeed * Time.deltaTime);
+    }
+  }
+
+
+  #region OldMovementCode
+  //if a condition is met,
+  //Do this things.
+  //else, do this other thing. 
+
+  //Vector3(x, y, z)
+
+  //if (Input.GetKey(KeyCode.D))
+  //{
+  //  Vector3 movementRight = new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+  //  transform.Translate(movementRight);
+  //}
+
+  // if (Input.GetKey(KeyCode.A)) 
+  //{ 
+  //   Vector3 movementLeft = new Vector3(-moveSpeed * Time.deltaTime, 0f, 0f);
+  //   transform.Translate(movementLeft);
+  //}
+
+  //if (Input.GetKey(KeyCode.W))
+  //{
+  //  Vector3 movementLeft = new Vector3(0f, 0f, moveSpeed * Time.deltaTime);
+  //  transform.Translate(movementLeft);
+  //}
+
+  //if (Input.GetKey(KeyCode.S))
+  //{
+  //  Vector3 movementLeft = new Vector3(0f, 0f, -moveSpeed * Time.deltaTime);
+  //  transform.Translate(movementLeft);
+  //}
+  #endregion
+
 }
